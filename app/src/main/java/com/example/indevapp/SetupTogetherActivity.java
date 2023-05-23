ee43661fe8f6114723eea56ea88c30eb1d74370c
YuhoLee <cdcdefg4@inbody.com>
2023-05-23 16:06:16 +0900

    commit create page

---
+++ app/src/main/java/com/example/indevapp/SetupTogetherActivity.java
@@ -0,0 +1,27 @@
+package com.example.indevapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class SetupTogetherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setuptogether);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
+    }
+
+    public void Click_button_save(View view) {
+        Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show();
+
+    }
+}
