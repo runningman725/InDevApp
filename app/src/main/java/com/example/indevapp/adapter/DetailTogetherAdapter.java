4b328e63691bfe37cf6fb404e47cc5b5e1b3e234
DESKTOP-E2HCUV1\admin <liqiming@inbody.com>
2023-05-23 22:38:26 +0800

    현황 투개더 commit

---
+++ app/src/main/java/com/example/indevapp/adapter/DetailTogetherAdapter.java
@@ -0,0 +1,110 @@
+package com.example.indevapp.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.indevapp.R;
+
+import org.w3c.dom.Text;
+
+public class DetailTogetherAdapter extends RecyclerView.Adapter<DetailTogetherAdapter.TogetherView> {
+
+    private Context context;
+    public DetailTogetherAdapter(Context context) {
+        this.context = context;
+
+    }
+
+    @NonNull
+    @Override
+    public DetailTogetherAdapter.TogetherView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = View.inflate(context, R.layout.item_detail_together, null);
+        return new DetailTogetherAdapter.TogetherView(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DetailTogetherAdapter.TogetherView holder, int position) {
+        Glide.with(context)
+                .load("https://www.eatingwell.com/thmb/m5xUzIOmhWSoXZnY-oZcO9SdArQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/article_291139_the-top-10-healthiest-foods-for-kids_-02-4b745e57928c4786a61b47d8ba920058.jpg")
+                .centerCrop()
+                .into(holder.iv_user);
+        holder.tv_user.setText("111");
+        holder.tv_weight_change.setText("222");
+        holder.tv_weight_rate.setText("333");
+        holder.tv_weight_muscle.setText("444");
+
+        int weightMinus=0; //데이터 api에서 획득하여 추가,절대치
+        int weightPlus=0;
+        int rateMinus=0;
+        int ratePlus=0;
+        int muscleMinus=0;
+        int musclePluls=0;
+
+        minusDataFunc(holder.iv_weight_minus,weightMinus);
+        minusDataFunc(holder.iv_rate_minus,rateMinus);
+        minusDataFunc(holder.iv_muscle_minus,muscleMinus);
+
+        plusDataFunc(holder.iv_weight_plus, weightPlus);
+        plusDataFunc(holder.iv_rate_plus, ratePlus);
+        plusDataFunc(holder.iv_muscle_plus, musclePluls);
+    }
+
+    private void minusDataFunc(ImageView imageView, int minus) {
+        ViewGroup.LayoutParams lp = imageView.getLayoutParams();
+        lp.width = minus*ConvertDPtoPX(context,100)/10;
+        imageView.setLayoutParams(lp);
+    }
+
+    private void plusDataFunc(ImageView imageView, int plus) {
+        ViewGroup.LayoutParams lp = imageView.getLayoutParams();
+        lp.width = plus*ConvertDPtoPX(context,100)/10;
+        imageView.setLayoutParams(lp);
+    }
+
+    public static int ConvertDPtoPX(Context context, int dp) {
+        float density = context.getResources().getDisplayMetrics().density;
+        return Math.round((float) dp * density);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class TogetherView extends RecyclerView.ViewHolder {
+        ImageView iv_user;
+        TextView tv_user;
+        TextView tv_weight_change;
+        TextView tv_weight_rate;
+        TextView tv_weight_muscle;
+        ImageView iv_weight_minus;
+        ImageView iv_weight_plus;
+        ImageView iv_rate_minus;
+        ImageView iv_rate_plus;
+        ImageView iv_muscle_minus;
+        ImageView iv_muscle_plus;
+
+        public TogetherView(@NonNull View itemView) {
+            super(itemView);
+            iv_user = itemView.findViewById(R.id.iv_user);
+            tv_user = itemView.findViewById(R.id.tv_user);
+            tv_weight_change = itemView.findViewById(R.id.tv_weight_change);
+            tv_weight_rate = itemView.findViewById(R.id.tv_weight_rate);
+            tv_weight_muscle = itemView.findViewById(R.id.tv_weight_muscle);
+            iv_weight_minus = itemView.findViewById(R.id.iv_weight_minus);
+            iv_weight_plus = itemView.findViewById(R.id.iv_weight_plus);
+            iv_rate_minus = itemView.findViewById(R.id.iv_rate_minus);
+            iv_rate_plus = itemView.findViewById(R.id.iv_rate_plus);
+            iv_muscle_minus = itemView.findViewById(R.id.iv_muscle_minus);
+            iv_muscle_plus = itemView.findViewById(R.id.iv_muscle_plus);
+        }
+    }
+}
--- app/src/main/java/com/example/indevapp/fragment/DetailRankFragment.java
+++ app/src/main/java/com/example/indevapp/fragment/DetailRankFragment.java
@@ -1,22 +1,83 @@
 package com.example.indevapp.fragment;
 
+import android.annotation.SuppressLint;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.indevapp.R;
 
 public class DetailRankFragment extends Fragment {
 
+    private static final int TOGETHER=0;
+    private static final int BODYRANK=1;
+    private int type=0;
+
+    private FrameLayout framelayout;
+
+    private DetailTogetherFragment detailTogetherFragment = new DetailTogetherFragment();
+    private DetailBodyrankFragment detailBodyrankFragment = new DetailBodyrankFragment();
+    private SwitchCompat switch_compat;
+    private Fragment fragment;
+
+    @SuppressLint("MissingInflatedId")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_detail_rank,container,false);
+        framelayout = view.findViewById(R.id.framelayout);
+        switch_compat = view.findViewById(R.id.switch_compat);
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        fragment = fm.findFragmentById(R.id.framelayout);
+        if (fragment != null) {
+            transaction.remove(fragment);
+            fm.popBackStack();
+            transaction.commit();
+        }
+//        fragment= new DetailTogetherFragment();
+//        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, fragment);
+        if (fragment == null) {
+            getActivity().getSupportFragmentManager().beginTransaction().add(R.id.framelayout,new DetailTogetherFragment()).commit();
+            Log.e("tag", "onCreateView: fragment111");
+        }
+        switch_compat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                hideAllFragments(transaction);
+                if(isChecked){
+                    fragment = new DetailBodyrankFragment();
+                    Toast.makeText(getContext(),"checked",Toast.LENGTH_LONG).show();
+                } else {
+                    fragment = new DetailTogetherFragment();
+                    Toast.makeText(getContext(),"not checked",Toast.LENGTH_LONG).show();
+                }
+
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, fragment).commit();
+            }
+        });
+
         return view;
     }
+
+    private void hideAllFragments(FragmentTransaction transaction) {
+        if (detailTogetherFragment != null) transaction.hide(detailTogetherFragment);
+        if (detailBodyrankFragment != null) transaction.hide(detailBodyrankFragment);
+    }
+
 }
