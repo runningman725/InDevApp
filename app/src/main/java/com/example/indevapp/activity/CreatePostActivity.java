6aa26f6d5e86c017075e1ef964e67079c3a4aef0
YuhoLee <cdcdefg4@inbody.com>
2023-05-24 01:17:43 +0900

    commit

--- app/src/main/java/com/example/indevapp/activity/CreatePostActivity.java
+++ app/src/main/java/com/example/indevapp/activity/CreatePostActivity.java
@@ -1,13 +1,16 @@
 package com.example.indevapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.indevapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CreatePostActivity extends AppCompatActivity {
 
@@ -20,9 +23,17 @@ public class CreatePostActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("");
+
+        initControl();
     }
 
-    private void Click_botton_create(View view) {
-
+    private void initControl() {
+        Button btn_createInBodyTogether = (Button) findViewById(R.id.button_create);
+        btn_createInBodyTogether.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(CreatePostActivity.this, "게시글을 추가하였습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
